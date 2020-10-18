30eb31fb8fca4df21ea98816fb67832f2b6f87c0
Ali Salman <eng.aliabdullah@gmail.com>
2020-10-18 03:13:40 +0000

    Adding new practice

---
+++ CountTheBlocks.java
@@ -0,0 +1,13 @@
+class pyramidBlocksCounter {
+  public static void main(String[] args) {
+    int results = blocksCounter(3);
+    System.out.println(results);
+  }
+  public static int blocksCounter(int levels) {
+    int blocksCount = 0;
+    for (int i = 1; i <= levels; i++) {
+      blocksCount = blocksCount + i^2; 
+    }
+    return blocksCount;
+  }
+}
